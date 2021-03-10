66767c4c9bfcaf27c997cbfbdad022416d2532e7
souko2525 <yusuke.h0808@gmail.com>
2021-03-10 21:33:08 +0900

    fix ci

--- src/ch1/knock02.rs
+++ src/ch1/knock02.rs
@@ -1,3 +1,5 @@
 pub fn patcartaxi(s1: &str, s2: &str) -> String {
-    s1.chars().zip(s2.chars()).map(|(c1, c2)| format!("{}{}", c1, c2)).collect::<String>()
+    s1.chars().zip(s2.chars())
+    .map(|(c1, c2)| format!("{}{}", c1, c2))
+    .collect::<String>()
 }
