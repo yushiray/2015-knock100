916dacc1869f229f9331d9e8ec4e94898031b508
souko2525 <yusuke.h0808@gmail.com>
2021-03-20 17:33:20 +0900

    add 012

--- src/ch2/knock10.rs
+++ src/ch2/knock10.rs
@@ -1,6 +1,9 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::path::Path;
 use std::io::{BufRead, BufReader};
+use std::io::prelude::*;
+
 use std::io;
 
 pub fn count_lines(path: &Path) -> usize {
@@ -23,4 +26,22 @@ pub fn tab_to_space2(path: &Path, tab_width: usize) -> io::Result<String> {
     let br = BufReader::new(file);
     let spaces = " ".repeat(tab_width);
     Ok(br.lines().map(|s| match s { Ok(s) => s.replace("\t", &spaces) + "\n", Err(_) => "\0".to_string() }).collect())
+}
+
+pub fn cut(input_file_name: &Path, num: usize, output_file_name: &str) {
+    let input_f = File::open(input_file_name).expect("file not found");
+    let read_buf = BufReader::new(input_f);
+    let mut output_f = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(output_file_name)
+        .expect(format!("can't open file[{}] with write option", output_file_name).as_str());
+    read_buf.lines().for_each(|line| match line {
+        Ok(line) => {
+            let columns: Vec<_> = line.split('\t').collect();
+            writeln!(output_f, "{}", columns[num]).unwrap();
+            output_f.flush().expect("Error during flush");
+        }
+        Err(_) => panic!("parse error "),
+    });
 }
